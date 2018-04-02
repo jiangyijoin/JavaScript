e40f6b38195b534a1b6b5d89d1c7adc7036dc6ef
e <819454817@qq.com>
2018-04-02 12:53:40 +0800

    首次提交

--- Modules/RequireJS/main.js
+++ Modules/RequireJS/main.js
@@ -5,7 +5,13 @@
 require.config({
     paths:{
         "jquery": "jquery.min"
+    },
+
+    //shim参数用来加载非amd规范的插件，类库
+    shim:{
+
     }
+
 })
 //define可以定义return返回，require不需要定义return
 // 所以第一次mainJs都可以用，但是本地modules得用define
@@ -15,6 +21,7 @@ requirejs(['jquery','add'],function ($,add){
     setTimeout(function (){
         //document.write会重新打开文档流，然后清除原先的dom。浏览器就显示write里面的东西。
         document.write(add.add(3,5));
+        //如果文档流没关闭再执行docment.write，会一直在后面追加
         document.close();
     },4000)
 })
